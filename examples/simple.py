9510a69d9ad63877daadb372d6d06f350beee435
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2024-03-28 22:32:44 +0800

    edits

---
+++ examples/simple.py
@@ -0,0 +1,18 @@
+__author__ = 'Robbert Harms'
+__date__ = '2020-04-07'
+__maintainer__ = 'Robbert Harms'
+__email__ = 'robbert@xkls.nl'
+__licence__ = 'GPL v3'
+
+
+from ybe import read_ybe_file, YbeToLatex, YbeToMarkdown, YbeToDocx, YbeToODT, YbeToHTML
+
+from importlib import resources
+
+ybe_exam = read_ybe_file('./simple.ybe')
+
+YbeToLatex().convert(ybe_exam, '/tmp/ybe/latex/main.tex', copy_resources=True)
+YbeToMarkdown().convert(ybe_exam, '/tmp/ybe/markdown/main.md', copy_resources=True)
+YbeToHTML().convert(ybe_exam, '/tmp/ybe/html/main.html', copy_resources=True)
+YbeToDocx().convert(ybe_exam, '/tmp/ybe/main.docx')
+YbeToODT().convert(ybe_exam, '/tmp/ybe/main.odt')
